55f0e1a6df653aacd8cbd57e24b9fa481a86e2af
Thisun_105210 <thisun.matheesha@axiatadigitallabs.com>
2022-09-29 10:42:38 +0530

    initial commit

---
+++ src/main/java/com/kuehne_nagel/city_list/CityListApplication.java
@@ -0,0 +1,13 @@
+package com.kuehne_nagel.city_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuehne_nagel/city_list/CityListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuehne_nagel.city_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
